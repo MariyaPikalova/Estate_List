70e9a0b90ef530cd29dce6eae2e2050717844742
MariyaPikalova <it.pikalova@gmail.com>
2019-03-11 11:06:34 +0200

    save clicked item to localStorage

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 let startButton = document.getElementById("start-search-button");
+let marksBtn = document.getElementById("marks-btn");
 let currentPage = 1;
+let totalPages = 0;
+let elementsOnPage = 10;
+
 
 startButton.addEventListener("click", startHandler);
 
@@ -14,22 +18,38 @@ function startHandler() {
 
 function callbackResult(result) {
   if (result.response != null && result.response != undefined) {
-    let containerForStartPage = document.getElementById("container-page");
-    containerForStartPage.className = "container d-none";
 
-    let containerForGalleryPage = document.getElementById("garbage");
+    var containerForGalleryPage = document.getElementById("list");
     containerForGalleryPage.className = "grid";
 
     currentPage = result.response.page;
     totalPages = result.response.total_pages;
+
     containerForGalleryPage.addEventListener("mouseover", hoverOnPhoto);
     containerForGalleryPage.addEventListener("mouseout", hoverOnPhoto);
 
     console.log(result.response);
 
-    for (let i = 0; i < result.response.listings.length; i++) {
+    for (var i = 0; i < result.response.listings.length; i++) {
+
       house = result.response.listings[i];
       renderImages(house, i, "item", containerForGalleryPage);
+
+    }
+    let addMarks = document.getElementsByClassName('add-marks');
+    console.log(addMarks);
+    for (let i = 0; i < addMarks.length; i++ ){
+      addMarks[i].addEventListener('click', function (event) {
+        for (var i = 0; i < result.response.listings.length; i++) {
+          if ( event.currentTarget === event.currentTarget.parentElement.childNodes[i]) {
+            var bookmarks = [];
+            bookmarks.push(result.response.listings[i]);
+            console.log(bookmarks);
+            localStorage.setItem('marks', JSON.stringify(bookmarks));
+            alert('Добавлено в закладки');
+          }
+        }
+      });
     }
   }
 }
@@ -39,31 +59,34 @@ function renderImages(house, number, className, mainDiv) {
   childDiv.className = className;
   childDiv.id = number;
   childDiv.style.backgroundImage = "url(" + house.img_url + ")";
-
   let shadowDiv = document.createElement('div');
+  let link = document.createElement('button');
+  link.className = 'add-marks';
   shadowDiv.className = 'shadow';
+  link.innerHTML = 'Add to marks';
   shadowDiv.innerHTML = house.price_formatted + '<br>';
   shadowDiv.innerHTML += house.title + '<br>';
   shadowDiv.innerHTML += house.summary + '<br>';
   shadowDiv.innerHTML += house.updated_in_days_formatted + '<br>';
 
   childDiv.appendChild(shadowDiv);
+  childDiv.appendChild(link);
   mainDiv.appendChild(childDiv);
 }
 
 function hoverOnPhoto(event) {
   if (event.target.className === "item") {
-    if (event.type == "mouseover") {
+    if (event.type === "mouseover") {
       event.target.childNodes[0].style.display = "block";
       for (let i = 0; i < event.target.parentElement.childNodes.length; i++) {
-        if (event.target != event.target.parentElement.childNodes[i]) {
+        if (event.target !== event.target.parentElement.childNodes[i]) {
           event.target.parentElement.childNodes[i].childNodes[0].style.display = "none";
         }
       }
     }
   }
   if (event.target.className === "shadow") {
-    if (event.type == "mouseout") {
+    if (event.type === "mouseout") {
       event.target.style.display = "none";
     }
   }
