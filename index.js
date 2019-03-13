2d6f16a5c8eadeae0045823037c2f274ece27452
MariyaPikalova <it.pikalova@gmail.com>
2019-03-13 13:17:51 +0200

    adding to favorites

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 let startButton = document.getElementById("start-search-button");
 let marksBtn = document.getElementById("marks-btn");
 let nextLoad = document.getElementById('next-load');
+let exitBtn = document.getElementById('exit-btn');
+let saveDiv = document.getElementById('show-marks');
+let hideContainer = document.getElementById('hide-content');
 
 
 startButton.addEventListener('click', startHandler);
 nextLoad.addEventListener('click', nextPage);
 marksBtn.addEventListener('click', showMarks);
+exitBtn.addEventListener('click', function (e) {
+  saveDiv.innerHTML = '';
+  saveDiv.style.display = 'none';
+  hideContainer.style.display = 'block';
+  exitBtn.style.display = 'none';
+});
 
 function startHandler() {
   let city = document.getElementById("city-input");
@@ -39,22 +48,6 @@ function callbackResult(result) {
       house = result.response.listings[i];
       renderImages(house, i, "item", containerForGalleryPage);
     }
-
-    let addMarks = document.getElementsByClassName('add-marks');
-    console.log(addMarks);
-    for (let i = 0; i < addMarks.length; i++ ){
-      addMarks[i].addEventListener('click', function (event) {
-        for (var i = 0; i < result.response.listings.length; i++) {
-          if ( event.currentTarget === event.currentTarget.parentElement.childNodes[i]) {
-            var bookmarks = [];
-            bookmarks.push(result.response.listings[i]);
-            console.log(bookmarks);
-            localStorage.setItem('marks', JSON.stringify(bookmarks));
-            alert('Добавлено в закладки');
-          }
-        }
-      });
-    }
    nextLoad.style.display = 'block';
   }
 }
@@ -62,22 +55,32 @@ function callbackResult(result) {
 function renderImages(house, number, className, mainDiv) {
   let childDiv = document.createElement('div');
   childDiv.className = className;
-  childDiv.id = number;
+  childDiv.id = `item-${number}`;
   childDiv.style.backgroundImage = "url(" + house.img_url + ")";
-  let shadowDiv = document.createElement('div');
+
   let link = document.createElement('button');
   link.className = 'add-marks';
+  link.id = `button-${number}`;
+  link.innerHTML = 'SAVE TO FAVORITES';
+
+  let shadowDiv = document.createElement('div');
   shadowDiv.className = 'shadow';
-  link.innerHTML = 'ADD TO MARKS';
   shadowDiv.innerHTML = house.price_formatted + '<br>';
   shadowDiv.innerHTML += house.title + '<br>';
   shadowDiv.innerHTML += house.summary + '<br>';
   shadowDiv.innerHTML += house.updated_in_days_formatted + '<br>';
 
-
   childDiv.appendChild(shadowDiv);
   childDiv.appendChild(link);
   mainDiv.appendChild(childDiv);
+
+  link.addEventListener('click', function (e) {
+    let itemId = childDiv.id.split('-');
+    let butId = link.id.split('-');
+    if (itemId[1]=== butId[1]){
+       let key = 'key'+ Date.now() + Math.random();
+       localStorage.setItem(key, childDiv.outerHTML);}
+  })
 }
 
 function hoverOnPhoto(event) {
@@ -99,6 +102,19 @@ function hoverOnPhoto(event) {
 }
 
 function showMarks() {
-  let hideContainer = document.getElementById('hide-content');
-  hideContainer.style.display = 'none';
+ hideContainer.style.display = 'none';
+ saveDiv.style.display = 'block';
+ exitBtn.style.display = 'inline-block';
+
+  for (let i=0;i<localStorage.length;i++){
+    let key = localStorage.key(i);
+    // console.log(key);
+    let res = localStorage.getItem(key);
+    console.log(res);
+    let containerForSavingPage = document.createElement('div');
+    containerForSavingPage.className = 'grid';
+    saveDiv.appendChild(containerForSavingPage);
+    containerForSavingPage.innerHTML = res;
+  }
 }
+
